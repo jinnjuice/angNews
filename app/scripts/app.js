135a338b1409e62257f0ca48a463cd76b8eec334
Sean <skim2636@gmail.com>
2014-05-12 17:55:47 -0700

    adding scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+/* global app:true */
+'use strict';
+
+var app = angular.module('angNewsApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+])
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/posts.html',
+            controller: 'PostsController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
